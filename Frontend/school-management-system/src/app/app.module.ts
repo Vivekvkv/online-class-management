2ceacb88d15a86ab3f93d726dd3dae7e286c740d
Vivekvkv <69642600+Vivekvkv@users.noreply.github.com>
2021-03-19 18:39:03 +0530

    register
    Registration Component with Angular Material

--- Frontend/school-management-system/src/app/app.module.ts
+++ Frontend/school-management-system/src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
+import { RegisterComponent, DialogView } from './register/register.component';
 import {MatButtonModule} from '@angular/material/button';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatCardModule} from '@angular/material/card';
@@ -13,6 +14,93 @@ import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
+import {MatDialogModule } from '@angular/material/dialog';
+
+import { CommonModule } from '@angular/common';
+import { OverlayModule } from '@angular/cdk/overlay';
+import { CdkTreeModule } from '@angular/cdk/tree';
+import { PortalModule } from '@angular/cdk/portal';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatRippleModule } from '@angular/material/core';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatTreeModule } from '@angular/material/tree';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatTooltipModule } from '@angular/material/tooltip';
+
+
+
+
+const modules = [
+  MatButtonModule,
+  MatFormFieldModule,
+  MatInputModule,
+  BrowserModule,
+    AppRoutingModule,
+    MatButtonModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatInputModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatDialogModule,
+    MatFormFieldModule,
+
+    CdkTreeModule,
+    MatAutocompleteModule,
+    MatButtonModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatDividerModule,
+    MatExpansionModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatProgressSpinnerModule,
+    MatPaginatorModule,
+    MatRippleModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSnackBarModule,
+    MatSortModule,
+    MatTableModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatButtonToggleModule,
+    MatTreeModule,
+    OverlayModule,
+    PortalModule,
+    MatBadgeModule,
+    MatGridListModule,
+    MatRadioModule,
+    MatDatepickerModule,
+    MatTooltipModule,
+    
+
+
+
+]
+
+
 
 
 @NgModule({
@@ -20,22 +108,12 @@ import {MatIconModule} from '@angular/material/icon';
     AppComponent,
     LoginComponent,
     RegisterComponent,
+    DialogView,
     
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    MatButtonModule,
-    BrowserAnimationsModule,
-    MatCardModule,
-    MatCheckboxModule,
-    MatInputModule,
-    MatFormFieldModule,
-    MatToolbarModule,
-    MatIconModule,
-    
-    
-  ],
+  imports: [...modules],
+  exports: [...modules],
+
   providers: [],
   bootstrap: [AppComponent]
 })
--- Frontend/school-management-system/src/app/register/register.component.ts
+++ Frontend/school-management-system/src/app/register/register.component.ts
@@ -1,15 +1,65 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup,FormControl, Validators } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { MatFormField} from '@angular/material/form-field';
 @Component({
   selector: 'app-register',
-  templateUrl: './register.component.html',
+  template: `
+  <h1>Hello</h1>
+<div>
+  <button mat-raised-button (click)="openDialog()">Signup</button>
+
+  </div>
+  
+  `,
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
+  ravi:string="123";
+  constructor(private dialog:MatDialog) { }
 
   ngOnInit(): void {
   }
+  openDialog(){
 
+    const dialogRef = this.dialog.open(DialogView);
+    var data = dialogRef.afterClosed().subscribe(() => this.ravi);
+    console.log(data)
+    
+    }
+    
+
+  }
+
+
+
+@Component({
+  selector:'dialog-view',
+  templateUrl:'./dialog-view.html',
+  styleUrls: ['./register.component.css']
+})
+
+export class DialogView implements OnInit {
+  myForm:any = FormGroup;
+  
+  constructor() { }
+
+  ngOnInit() {
+
+    this.myForm = new FormGroup({
+        'firstName': new FormControl('', Validators.required)
+    });
+
+
+  }
+  Roles: any = ['Admin', 'Author', 'Reader'];
+  email = new FormControl('',[Validators.required])
+  
+  getErrorMessage(){
+    if(this.email.hasError('required'))
+    {
+      return "You must Enter Email Addres!!";
+    }
+    return this.email.hasError('email') ? 'Enter valid Email' : '';
+  }
 }
